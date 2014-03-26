b7d6266c6bac6ae37311a967c33ac9028b1d078e
Gaurav Dadhania <gaurav@dadhania.in>
2014-03-26 23:33:15 +1100

    Crazier experimentation with the Hook resource.

--- dzapgo/dzapgo/resources.py
+++ dzapgo/dzapgo/resources.py
@@ -3,10 +3,25 @@ from tastypie import fields
 from tastypie.resources import ModelResource
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
+
+from django.contrib.auth import get_user_model
+
 from rest_hooks.models import Hook
 
+
+User = get_user_model()
+
+class UserResource(ModelResource):
+    class Meta:
+        resource_name = 'user'
+        queryset = User.objects.all()
+        authentication = Authentication()
+        authorization = Authorization()
+        allowed_methods = ['get']
+
+
 class HookResource(ModelResource):
-    user = fields.ToOneField(default=1)
+    user = fields.ToOneField(UserResource, 'user', default=1)
 
     class Meta:
         resource_name = 'hooks'
