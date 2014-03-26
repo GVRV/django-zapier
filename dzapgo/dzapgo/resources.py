c1119ff87524ed357b5bba8b538cdc942ef3b022
Gaurav Dadhania <gaurav@dadhania.in>
2014-03-26 23:19:29 +1100

    Make the hook resource a free-for-all.

--- dzapgo/dzapgo/resources.py
+++ dzapgo/dzapgo/resources.py
@@ -1,10 +1,14 @@
 
 from tastypie.resources import ModelResource
+from tastypie.authentication import Authentication
+from tastypie.authorization import Authorization
 from rest_hooks.models import Hook
 
 class HookResource(ModelResource):
     class Meta:
         resource_name = 'hooks'
         queryset = Hook.objects.all()
+        authentication = Authentication()
+        authorization = Authorization()
         allowed_methods = ['get', 'post', 'delete']
         fields = ['event', 'target']
