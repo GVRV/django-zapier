0055e756d91efd8bec7942351293817316c179ae
Gaurav Dadhania <gaurav@dadhania.in>
2014-03-26 23:28:29 +1100

    Testing a crazy experiment with Hook resource user association.

--- dzapgo/dzapgo/resources.py
+++ dzapgo/dzapgo/resources.py
@@ -1,10 +1,13 @@
 
+from tastypie import fields
 from tastypie.resources import ModelResource
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
 from rest_hooks.models import Hook
 
 class HookResource(ModelResource):
+    user = fields.ToOneField(default=1)
+
     class Meta:
         resource_name = 'hooks'
         queryset = Hook.objects.all()
